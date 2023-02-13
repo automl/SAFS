33478db0dac443a114b24d84eb3a79d445fa06a0
amsks <adityak735@gmail.com>
2023-02-13 14:43:02 +0100

    initialized templates

---
+++ safs/__init__.py
@@ -0,0 +1,14 @@
+import datetime
+
+name = "SAFS"
+package_name = "safs"
+author = "amsks and Mohammadloni and 0mehdi0"
+author_email = "adityak735@gmail.com"
+description = "A repository for sparse Activation Funtion search"
+url = "https://www.automl.org"
+project_urls = {
+    "Documentation": "https://automl.github.io/SAFS/main",
+    "Source Code": "https://github.com/automl/safs",
+}
+copyright = f"Copyright {datetime.date.today().strftime('%Y')}, amsks and Mohammadloni and 0mehdi0"
+version = "0.0.1"

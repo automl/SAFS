efdf58e77e32fa7e94a1f1b89a35da98809f4507
amsks <adityak735@gmail.com>
2023-02-13 15:31:48 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -52,12 +52,19 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8",
     install_requires=[
-        "numpy"
+        "numpy== 1.24.2",
+        "torch==1.13.1",
+        "torchvision==0.14.1",
+        "tensorboard==2.12.0",
+        "simanneal==0.5.0",
+        "lahc==1.0.5",
+        "pyeasyga==0.3.1"  
     ],
     extras_require=extras_require,
     test_suite="pytest",
     platforms=["Linux"],
     classifiers=[
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
